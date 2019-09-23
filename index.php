79b784ab47f82ef1002bbc50d3698fe4dd47c927
patrickrabano <patdapogi@gmail.com>
2019-09-23 12:52:24 +0800

    aaa

--- index.php
+++ index.php
@@ -157,7 +157,7 @@
             <a href="./index.php">
                 <img class="navbar-brand" src="./assets/images/portal_logo.png" width="50px" height="58px"/>
             </a>
-            <p class="title">AAA</p>        </nav>
+            <p class="title">das</p>        </nav>
         <div class="container">
             <div class="portal-content">
                 <div class="row">
